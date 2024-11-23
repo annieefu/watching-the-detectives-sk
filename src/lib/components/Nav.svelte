eb36e30bd584c69186a133b0011d0592afb6549c
annieefu <annieccfu@gmail.com>
2024-11-23 00:03:41 -0500

    first file migration

---
+++ src/lib/components/Nav.svelte
@@ -0,0 +1,9 @@
+
+<nav>
+    <ul>
+      <li><a href="/">🔎 home</a></li>
+      <li><a href="about.html">🌃 about this project</a></li>
+      <li><a href="awards.html">🏆 awards/honors</a></li>
+      <li><a href="reviews.html">⭐ leave a review</a></li>
+    </ul>
+  </nav>
---
+++ src/routes/about/+page.svelte
@@ -0,0 +1,84 @@
+<script>
+
+  </script>
+  
+    <!-- <div class="gradient"></div> -->
+  
+    <section class="layout">
+      <!--  -->
+      <div class="body-content">
+        <div class="header">
+          <h1>✨Watching the Detectives Fan Club HQ✨</h1>
+        </div>
+  
+        <div class="main">
+          <div class="summary">
+            <p>
+              This project was born on an early morning in September 2023, after
+              we (Annie Fu and Dana Gong) watched the incredible movie that is
+              Watching the Detectives. We finished it around 3am, delirious from
+              constant laughter and bewildered at what we had just seen. We loved
+              it. We didn't understand how the movie didn't have a cult following.
+              We were determined to kickstart said cult following on our own. Over
+              the next few months, we fleshed out the idea for this fansite and
+              worked on it here and there, forming a consistent throughline for
+              our friendship.
+            </p>
+  
+            <p>
+              Dana passed away in the summer of 2024, before we could finish the
+              site.
+            </p>
+            <br /><br /><br />
+            <div class="quote">
+              <p>
+                <strong
+                  >Because true collaborators in this life are rare.
+                </strong><br />
+                <span class="quote-source">
+                  — Gabrielle Zevin, <i>Tomorrow and Tomorrow and Tomorrow</i
+                  ></span
+                >
+              </p>
+            </div>
+  
+            <p>
+              <span class="tooltip"
+                >Neil (our fav Cillian Murphy)<span>
+                  <img class="tooltip" alt="" src="./assets/thumbsup.gif" />
+                </span></span
+              >
+              "an incorrigible prankster and film noir buff" breaks up with his
+              girlfriend citing her lack of similarity to
+              <span class="tooltip"
+                >Katherine Ross in "Butch Cassidy and the Sundance Kid."<span
+                  ><img
+                    class="tooltip"
+                    alt=""
+                    src="https://static.life.com/wp-content/uploads/2019/09/30171245/3.-1968_003619-1.png"
+                  /></span
+                ></span
+              >
+              She tells him to get over his silly little life pretending to live in
+              the movies through running an unprofitable video store. In walks
+              <span class="tooltip"
+                >Violet (also our fav Lucy Liu)<span
+                  ><img class="tooltip" alt="" src="imgs/lucy_liu.jpg" /></span
+                ></span
+              >, a modern day femme fatale who "starts to lead Neil down a road of
+              petty crime." Curiosity killed the cat they said. But it probably
+              wouldn't kill Neil to live in the moment, and Neil is not a cat.
+            </p>
+          </div>
+        </div>
+      </div>
+      <div class="footer"></div>
+    </section>
+ 
+  <style>
+    .quote-source {
+      margin-left: 40%;
+      text-align: right;
+    }
+  </style>
+  
