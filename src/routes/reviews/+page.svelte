de5d79c6b6e8cabdf41f76f17b7eab9f4e6bcd71
annieefu <annieccfu@gmail.com>
2025-05-01 13:41:32 -0400

    spacing

--- src/routes/reviews/+page.svelte
+++ src/routes/reviews/+page.svelte
@@ -79,7 +79,7 @@
 	<div class="review-elements">
 		<p>
 			No fan page is complete without hearing from the members themselves. Since this <i>is</i> the best movie of all time,
-			there are countless voices to hear from, all testifying to greatness of this movie. This is also a space to reflect
+			there are countless voices to hear from, all testifying to its greatness. This is also a space to reflect
 			and leave a message for Dana, if you'd like. Read through them below, or <a href="#submit-review">submit your own review
 			at the bottom of this page.</a>
 		</p>
